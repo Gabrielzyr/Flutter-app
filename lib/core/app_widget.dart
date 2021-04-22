da2487fa7c1e55bb4e0663f8614d43f4202e77ec
Gabrielzyr <gabrielmz397@gmail.com>
2021-04-22 18:54:04 -0300

    Added splash screen and HomePage structure

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,5 +1,5 @@
 // import 'package:DevQuiz/home/home_page.dart';
-// import 'package:nlw4_flutter/splash/splash_page.dart';
+import 'package:nlw5_flutter/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
@@ -7,25 +7,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("NLW FLutter"),
-        ),
-        body: Center(
-          child: Container(
-            decoration: BoxDecoration(color: Colors.purple),
-            child: Text(
-              "Ola mundo flutter", 
-              style: TextStyle(fontSize: 30)
-            ),
-          )
-          
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add)
-        ),
-      ),
+      home: SplashPage(),
     );
   }
 }
