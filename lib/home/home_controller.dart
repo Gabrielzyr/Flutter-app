3cdb5e2231ae232834b3604dbd1d6c8c91ea4a50
Gabrielzyr <gabrielmz397@gmail.com>
2021-04-24 18:25:08 -0300

    Added home repository

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -1,42 +1,31 @@
 import 'package:nlw5_flutter/core/app_images.dart';
+import 'package:nlw5_flutter/home/home_repository.dart';
 import 'package:nlw5_flutter/home/home_state.dart';
 import 'package:nlw5_flutter/shared/models/awnser_model.dart';
 import 'package:nlw5_flutter/shared/models/question_model.dart';
 import 'package:nlw5_flutter/shared/models/quiz_model.dart';
 import 'package:nlw5_flutter/shared/models/user_model.dart';
+import 'package:flutter/foundation.dart';
 
 class HomeController {
-  HomeState state = HomeState.empty;
+  final stateNotifier = ValueNotifier<HomeState>(HomeState.empty);
+  set state(HomeState state) => stateNotifier.value = state;
+  HomeState get state => stateNotifier.value;
 
   UserModel? user;
   List<QuizModel>? quizzes;
 
-  void getUser() {
-    user = UserModel(
-      name: "José",
-      photoUrl:
-          "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    );
+  final repository = HomeRepository();
+
+  void getUser() async {
+    state = HomeState.loading;
+    user = await repository.getUser();
+    state = HomeState.success;
   }
 
-  void getQuizzes() {
-    quizzes = [
-      QuizModel(
-        title: "NLW 5 Flutter",
-        imagem: AppImages.blocks,
-        level: Level.facil,
-        questions: [
-          QuestionModel(
-            title: "Está curtindo o Flutter?",
-            awnsers: [
-              AwnserModel(title: "Estou curtindo"),
-              AwnserModel(title: "Amando Flutter"),
-              AwnserModel(title: "Muito top", isRight: true),
-              AwnserModel(title: "Show de bola"),
-            ],
-          )
-        ],
-      )
-    ];
+  void getQuizzes() async {
+    state = HomeState.loading;
+    quizzes = await repository.getQuizzes();
+    state = HomeState.success;
   }
 }
