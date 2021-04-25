684d15d8bda161cac7551a6c8e26da2388512bb7
Gabrielzyr <gabrielmz397@gmail.com>
2021-04-25 17:53:49 -0300

    Added navigation between anwsers

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:nlw5_flutter/challenge/widgets/awnser/awnser_widget.dart';
 import 'package:nlw5_flutter/core/core.dart';
+import 'package:nlw5_flutter/shared/models/awnser_model.dart';
 import 'package:nlw5_flutter/shared/models/question_model.dart';
 
-class QuizWidget extends StatelessWidget {
+class QuizWidget extends StatefulWidget {
   final QuestionModel question;
-  const QuizWidget({Key? key, required this.question}) : super(key: key);
+  final VoidCallback onChange;
+  const QuizWidget({
+    Key? key,
+    required this.question,
+    required this.onChange,
+  }) : super(key: key);
 
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int? indexSelected = -1;
+
+  AwnserModel awnser(int index) => widget.question.awnsers[index];
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,20 +30,25 @@ class QuizWidget extends StatelessWidget {
             height: 64,
           ),
           Text(
-            question.title,
+            widget.question.title,
             style: AppTextStyles.heading,
           ),
           SizedBox(
             height: 24,
           ),
-          ...question.awnsers
-              .map(
-                (e) => AwnserWidget(
-                  isRight: e.isRight,
-                  title: e.title,
-                ),
-              )
-              .toList(),
+          for (var i = 0; i < widget.question.awnsers.length; i++)
+            AwnserWidget(
+              awnser: awnser(i),
+              disabled: indexSelected != -1,
+              isSelected: indexSelected == i,
+              onTap: () {
+                indexSelected = i;
+                widget.onChange();
+                setState(() {});
+                Future.delayed(Duration(seconds: 2))
+                    .then((value) => widget.onChange());
+              },
+            ),
         ],
       ),
     );
